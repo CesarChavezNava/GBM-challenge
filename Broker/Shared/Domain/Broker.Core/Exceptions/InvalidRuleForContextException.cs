8c9ecdd628ae14348ac43a6557ceec7c1ad31650
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 11:38:29 -0600

    feat: add contract for generating policies

---
+++ Broker/Shared/Domain/Broker.Core/Exceptions/InvalidRuleForContextException.cs
@@ -0,0 +1,8 @@
+﻿namespace Broker.Core.Exceptions;
+
+public class InvalidRuleForContextException : InvalidOperationException
+{
+    public InvalidRuleForContextException(string ruleName)
+        : base ($"The rule {ruleName} can't be executed for the current context.")
+    { }
+}
---
+++ Broker/Shared/Domain/Broker.Core/Rules/Policy.cs
@@ -0,0 +1,31 @@
+﻿using Broker.Core.Exceptions;
+
+namespace Broker.Core.Rules;
+
+/// <summary>
+/// Base contract to generate a policy.
+/// </summary>
+/// <typeparam name="TInput">Type of entity that will pass through the policy.</typeparam>
+public abstract class Policy<TInput>
+{
+    public BusinessErrors Execute(TInput entity, IBusinessRule<TInput>[] rules)
+    {
+        bool canBeExecuted = true;
+        BusinessErrors errors = new();
+        
+        foreach (var rule in rules) 
+        {
+            if (rule.ContextsToExecute is not null)
+                canBeExecuted = ValidateContexts(entity, rule);
+
+            if (!canBeExecuted)
+                throw new InvalidRuleForContextException(rule.GetType().Name);
+
+            rule.Execute(entity, errors);
+        }
+
+        return errors;
+    }
+
+    protected abstract bool ValidateContexts(TInput entity, IBusinessRule<TInput> rule);
+}
