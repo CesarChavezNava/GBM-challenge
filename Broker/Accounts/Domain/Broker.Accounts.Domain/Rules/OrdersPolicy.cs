7f42ab1a842edc38796b0d8c96b82f919898238f
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-15 00:02:42 -0600

    feat: add policy to orders

---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain/Rules/OrdersPolicy.cs
@@ -0,0 +1,23 @@
+﻿using Broker.Accounts.Domain.Entities.Write;
+using Broker.Accounts.Domain.Enums;
+using Broker.Core.Rules;
+
+namespace Broker.Accounts.Domain.Rules;
+
+public class OrdersPolicy : Policy<WriteOrder>
+{
+    protected override bool ValidateContexts(WriteOrder order, IBusinessRule<WriteOrder> rule)
+    {
+        if (rule.ContextsToExecute is null)
+            return true;
+
+        foreach (string context in rule.ContextsToExecute)
+        {
+            OperationCode currentContext = (OperationCode)Enum.Parse(typeof(OperationCode), context);
+            if (order.Operation.Value != currentContext)
+                return false;
+        }
+
+        return true;
+    }
+}
