725c6260f1e0915f967ee68d185264b3f26c2cfc
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 23:17:49 -0600

    feat: add a rule to validate that all operations must happen between 6am and 3pm

---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain/Rules/ClosedMarketRule.cs
@@ -0,0 +1,22 @@
+﻿using Broker.Accounts.Domain.Entities.Write;
+using Broker.Core.Rules;
+
+namespace Broker.Accounts.Domain.Rules;
+
+/// <summary>
+/// All operations must happen between 6am and 3pm.
+/// </summary>
+public class ClosedMarketRule : BusinessRule<WriteOrder>
+{
+    public ClosedMarketRule()
+        : base("CLOSED_MARKET")
+    { }
+
+    protected override bool Validate(WriteOrder order)
+    {
+        DateTime epoch = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        DateTime date = epoch.AddMilliseconds(order.Timestamp.Value).ToLocalTime();
+
+        return date.Hour >= 6 && date.Hour <= 15;
+    }
+}
