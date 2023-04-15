3f158606bb99e1cfea717f808c39879926447ed6
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 18:01:10 -0600

    feat: add use case create account

---
+++ Broker/Accounts/Application/Broker.Accounts.Application/Create/AccountCreator.cs
@@ -0,0 +1,20 @@
+﻿using Broker.Accounts.Domain.Entities.Read;
+using Broker.Accounts.Domain.Entities.Write;
+using Broker.Accounts.Domain.Repositories;
+
+namespace Broker.Accounts.Application.Create;
+
+public class AccountCreator : IForCreateAccount
+{
+    private readonly IAccountRepository accountRepository;
+
+    public AccountCreator(IAccountRepository accountRepository)
+    {
+        this.accountRepository = accountRepository;
+    }
+
+    public async Task<Account> Create(WriteAccount account)
+    {
+        return await accountRepository.Create(account);
+    }
+}
