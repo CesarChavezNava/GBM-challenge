1c1d9149924acc6b233aa5c42f785f6a94f63022
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 23:08:57 -0600

    feat: add rule to validate if there are sufficient funds to carry out the order

---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain/Rules/InsufficientBalanceRule.cs
@@ -0,0 +1,21 @@
+﻿using Broker.Accounts.Domain.Entities.Read;
+using Broker.Accounts.Domain.Entities.Write;
+using Broker.Core.Rules;
+
+namespace Broker.Accounts.Domain.Rules;
+
+/// <summary>
+/// When buying stocks, you must have enough cash in order to fulfill it.
+/// </summary>
+public class InsufficientBalanceRule : DependentBusinessRule<WriteOrder, Balance>
+{
+    public InsufficientBalanceRule(Balance currentBalance)
+        : base("INSUFFICIENT_BALANCE", additionalData: currentBalance, contextsToExecute: new string[] { "BUY" })
+    { }
+
+    protected override bool Validate(WriteOrder order, Balance currentBalance)
+    {
+        decimal total = order.TotalShares.Value * order.SharePrice.Value;
+        return currentBalance.Cash.Value >= total;
+    }
+}
