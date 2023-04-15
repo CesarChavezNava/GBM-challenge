e85588c1ba09fe627fad553396263aafb90194f2
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 23:00:12 -0600

    test: add test cases to value objects and entities for orders

---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain.Tests/Entities/Write/WriteOrderTests.cs
@@ -0,0 +1,61 @@
+﻿using Broker.Accounts.Domain.Entities.Write;
+using Broker.Accounts.Domain.Enums;
+using Broker.Accounts.Domain.Exceptions;
+using Broker.Accounts.Domain.ValueObjects;
+
+namespace Broker.Accounts.Domain.Tests.Entities.Write;
+
+[TestFixture]
+public class WriteOrderTests
+{
+    private WriteOrder expected;
+
+    [SetUp]
+    public void SetUp()
+    {
+        expected = new(
+            new UserId(1),
+            new Timestamp(1681840800000),
+            new Operation(OperationCode.BUY),
+            new IssuerName("MSFT"),
+            new TotalShares(1),
+            new SharePrice(5174.99m)
+        );
+    }
+
+    [Test(Description = "Invalid WriteOrder because the share price is 0, should return TooLowSharePriceException")]
+    public void OrderWithSharePrice0()
+    {
+        Action action = () => new WriteOrder(
+            new UserId(1),
+            new Timestamp(1681840800000),
+            new Operation(OperationCode.BUY),
+            new IssuerName("MSFT"),
+            new TotalShares(1),
+            new SharePrice(0)
+        );
+
+        action.Should().Throw<TooLowSharePriceException>();
+    }
+
+    [Test(Description = "Valid WriteOrder, should return expected")]
+    public void ValidWriteAccount()
+    {
+        WriteOrder order = new(
+            new UserId(1),
+            new Timestamp(1681840800000),
+            new Operation(OperationCode.BUY),
+            new IssuerName("MSFT"),
+            new TotalShares(1),
+            new SharePrice(5174.99m)
+        );
+
+        order.Should().NotBeNull();
+        order.UserId.Value.Should().Be(expected.UserId.Value);
+        order.Timestamp.Value.Should().Be(expected.Timestamp.Value);
+        order.Operation.Value.Should().Be(expected.Operation.Value);
+        order.IssuerName.Value.Should().Be(expected.IssuerName.Value);
+        order.TotalShares.Value.Should().Be(expected.TotalShares.Value);
+        order.SharePrice.Value.Should().Be(expected.SharePrice.Value);
+    }
+}
---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain.Tests/ValueObjects/TimestampTests.cs
@@ -0,0 +1,40 @@
+﻿using Broker.Accounts.Domain.Exceptions;
+using Broker.Accounts.Domain.ValueObjects;
+
+namespace Broker.Accounts.Domain.Tests.ValueObjects;
+
+[TestFixture]
+public class TimestampTests
+{
+    private Timestamp expected;
+
+    [SetUp]
+    public void SetUp()
+    {
+        long date_2023_04_18_12_00_00 = 1681840800000;
+        expected = new(date_2023_04_18_12_00_00);
+    }
+
+
+    [Test(Description = "Set a negative timestamp, should return TimestampOutOfRangeException")]
+    public void NegativeTimestamp()
+    {
+        Action action = () => new Timestamp(-1681815600000);
+        action.Should().Throw<TimestampOutOfRangeException>();
+    }
+
+    [Test(Description = "Set a timestamp in 0, should return TimestampOutOfRangeException")]
+    public void TimestampAs0()
+    {
+        Action action = () => new Timestamp(0);
+        action.Should().Throw<TimestampOutOfRangeException>();
+    }
+
+    [Test(Description = "Set timestamp in TIME, should return expected")]
+    public void TimestampInTime()
+    {
+        Timestamp timestamp = new(1681840800000);
+        timestamp.Should().NotBeNull();
+        timestamp.Value.Should().Be(expected.Value);
+    }
+}
