804e5b5efe49f9116ec8a13a73a151830785f517
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-14 23:14:43 -0600

    feat: add a rule to when selling stocks, you must have enough stocks in order to fulfill it

---
+++ Broker/Accounts/Domain/Broker.Accounts.Domain/Rules/InsufficientStocksRule.cs
@@ -0,0 +1,26 @@
+﻿using Broker.Accounts.Domain.Entities.Read;
+using Broker.Accounts.Domain.Entities.Write;
+using Broker.Core.Rules;
+
+namespace Broker.Accounts.Domain.Rules;
+
+/// <summary>
+/// When selling stocks, you must have enough stocks in order to fulfill it
+/// </summary>
+public class InsufficientStocksRule : DependentBusinessRule<WriteOrder, Balance>
+{
+	public InsufficientStocksRule(Balance currentBalance)
+		: base("INSUFFICIENT_STOCKS", additionalData: currentBalance, contextsToExecute: new string[] { "SELL" })
+	{ }
+
+    protected override bool Validate(WriteOrder order, Balance currentBalance)
+    {
+        Issuer? issuer =
+            currentBalance.Issuers.FirstOrDefault(issuer => issuer.IssuerName.Value.Equals(order.IssuerName.Value));
+
+        if (issuer is null)
+            return false;
+
+        return issuer.TotalShares.Value >= order.TotalShares.Value;
+    }
+}
