33df1c1a556b74ee626375cf1492387251f06d7f
Cesar Nava <ing.cesarchaveznava@gmail.com>
2023-04-17 02:07:35 -0600

    feat: remove minutes ago parameter from endpoint for getting orders

--- Broker/Accounts/Infrastructure/Broker.Accounts.Infrastructure.API/Dtos/SearchOrdersDto.cs
+++ Broker/Accounts/Infrastructure/Broker.Accounts.Infrastructure.API/Dtos/SearchOrdersDto.cs
@@ -4,7 +4,6 @@ public class SearchOrdersDto
 {
     public string? IssuerName { get; set; } = null;
     public string? Opeartion { get; set; } = null;
-    public int? MinuteAgo { get; set; } = 9999;
     public string? Order { get; set; } = null;
     public int? Limit { get; set; } = null;
     public int? Offset { get; set; } = null;
